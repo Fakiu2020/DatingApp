e43ad9535c90679f67a48d70e21a157c9586443b
facundo <facundomierez1@hotmail.com>
2019-04-15 20:26:42 -0300

    member list page

--- DattingApp-SPA/src/app/member-list/member-list.component.ts
+++ DattingApp-SPA/src/app/member-list/member-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { UserService } from '../services/user.service';
+import { AlertifyService } from '../services/alertify.service';
+import { ActivatedRoute } from '../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-member-list',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor(private userService: UserService, private alertify: AlertifyService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // this.route.data.subscribe(data => {
+    //   this.users = data['users'];
+    // });
+  this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService.getUsers().subscribe((users: User[]) => {
+      this.users = users;
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 
 }
